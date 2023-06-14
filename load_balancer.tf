90881e34934f18e2ddde923cdf917a02224939fd
tedkatapetrov1 <teodor.petrov@abv.bg>
2023-06-14 13:32:37 +0300

    new changes in var and lb files

--- load_balancer.tf
+++ load_balancer.tf
@@ -1,6 +1,6 @@
 # Create a Load Balancer ------------------------------------------------------
 resource "aws_lb" "my_load_balancer" {
-  name               = "my-load-balancer"
+  name               = var.lb_name
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.allow_http.id]
@@ -15,7 +15,7 @@ resource "aws_lb" "my_load_balancer" {
 # Create a Load Balancer Target Group -----------------------------------------
 
 resource "aws_lb_target_group" "my_alb_target_group" {
-  name     = "my-alb-target-group"
+  name     = var.lbtargetgroup_name
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.my_vpc_01.id
